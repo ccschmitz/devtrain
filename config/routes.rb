cce2d2bb358fc91d5846b7326b3ceaa0490b8564
Chris Schmitz <ccschmitz@gmail.com>
2014-10-10 08:38:08 -0500

    Setup homepage content

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :series, only: [:index, :show]
   resources :topics, only: [:index, :show]
 
+  get :about, to: 'pages#about'
+  get :faq, to: 'pages#faq'
+  get :requests, to: redirect('http://uservoice.com')
+  get :support, to: redirect('http://intercom.io')
+
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
 
